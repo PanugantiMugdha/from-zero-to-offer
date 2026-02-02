5071531884ec96a93fa7689724e0b1f74ed31ea8
Panuganti Mugdha <144349731+PanugantiMugdha@users.noreply.github.com>
2026-02-02 20:10:42 +0530

    Create AssignmentQn2.java

---
+++ Arrays Part-II/AssignmentQn2.java
@@ -0,0 +1,45 @@
+public class AssignmentQn2 
+{
+    public static int binarySearchIndex(int nums[], int target)
+    {
+        int left=0;
+        int right=nums.length-1;
+        while(left<=right)
+        {
+            int mid=(left+right)/2;
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            if(nums[left]<=nums[mid])
+            {
+                if(target>=nums[left] && target<nums[mid])
+                {
+                    right=mid-1;
+                }
+                else
+                {
+                    left=mid+1;
+                }
+            }
+            else
+            {
+                if(target>nums[mid] && target<=nums[right])
+                {
+                    left=mid+1;
+                }
+                else
+                {
+                    right=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int nums[]={4,5,6,7,1};
+        int target=7;
+        int result=binarySearchIndex(nums, target);
+        System.out.println(result);
+    }
+}
